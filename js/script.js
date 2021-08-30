95a58fe8f5edd96fdda15fc406b0d8455b66495e
Кокин Вадим <vadim.kokin.1999@mail.ru>
2021-08-30 18:00:48 +0400

    1024px adaptation finish

--- js/script.js
+++ js/script.js
@@ -4,6 +4,21 @@ $('.slider').slick();
 
   $('.slider').slick({
     infinite: true,
+    speed:300,
+    // опред. скорость пролистывания
     slidesToShow: 5,
-    slidesToScroll: 1
+    // кол-во слайдов для показа
+    slidesToScroll: 1,
+    // кол-во слайдов за раз перелистывается
+    responsive: [
+      {
+        breakpoint: 1025,
+        // сообщает, при какой ширине экрана нужно вкл. настройки
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true
+        }
+      }
+    ]
   });
